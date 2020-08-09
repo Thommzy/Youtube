dd2c6866bbb2299ed0228ed52912a5413320a557
Thommzy <tomiwaobeisun@yahoo.com>
2020-08-09 14:24:35 +0100

    addedfirebase

---
+++ clone-youtube/src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp1isBqJfYwK3PC0vWRnXiToH5wI-zRKo",
+  authDomain: "clone-6039c.firebaseapp.com",
+  databaseURL: "https://clone-6039c.firebaseio.com",
+  projectId: "clone-6039c",
+  storageBucket: "clone-6039c.appspot.com",
+  messagingSenderId: "724265492185",
+  appId: "1:724265492185:web:4f675cafaaba734fdae8c8",
+  measurementId: "G-HXFKEMTYWM",
+};
