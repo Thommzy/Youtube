6db54fd175729cdcdb37168a154baebf5a4679f8
Thommzy <tomiwaobeisun@yahoo.com>
2020-08-09 23:56:33 +0100

    Header 30% done

--- clone-youtube/src/Header/Header.js
+++ clone-youtube/src/Header/Header.js
@@ -1,9 +1,35 @@
 import React from "react";
+import "./css/Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from "../images/youtube.png";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallSharpIcon from "@material-ui/icons/VideoCallSharp";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { Avatar } from "@material-ui/core";
 
 function Header() {
   return (
     <div className="header">
-      <h1>I am a header</h1>
+      <div className="header__left">
+        <MenuIcon />
+        <img className="header__logo" src={logo} alt="youtube_logo" />
+      </div>
+
+      <div className="header__input">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__icons">
+        <VideoCallSharpIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar
+          alt="youtube_profile"
+          src="https://yt3.ggpht.com/a-/AOh14GiKEZmfLsC66e5E8dJPhJPjuKe9Ld0Yd-Dc0-alRw=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+      </div>
     </div>
   );
 }
